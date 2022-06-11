2a17a122a9707f8239c09a89e154590b0f37b536
matymcleod <matymcleod@gmail.com>
2022-06-10 20:26:20 -0700

    removing extra avatars in nav, adding cards to body

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 // Rule #1 (optional)
 import React from 'react';
+import App from '../App';
 
 
 // Rule #2 -- Define a function with same name as component name
@@ -7,56 +8,27 @@ import React from 'react';
 const Navbar = function() {
 // Rule #4 -- function/component needs to return something like HTML
   return (
-//     <div>
-//       <head>
-//         <link href="https://cdn.jsdelivr.net/npm/daisyui@2.11.0/dist/full.css" rel="stylesheet" type="text/css" />
-//         <script src="https://cdn.tailwindcss.com"></script>
-//       </head>
-//       <nav>
-//       <div class="navbar bg-base-100">
-//   <div class="flex-1">
-//     <p class="btn btn-ghost normal-case text-xl">Decosmos Housing Co-op</p>
-//   </div>
-//   <div class="flex-none">
-//     <ul class="menu menu-horizontal p-0">
-//       <li><a>About</a></li>
-//       <li tabindex="0">
-//         <a>
-//           Members
-//           <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
-//         </a>
-//         <ul class="p-2 bg-base-100">
-//           <li><a href='https://decosmos.myqnapcloud.com:8081/RepairRequest/StandardsInput.php' >Request Forms</a></li>
-//           <li><a >Committees</a></li>
-//         </ul>
-//       </li>
-//       <li><a>News</a></li>
-//     </ul>
-//   </div>
-// </div>
-//       </nav>
-//     </div>
-  // )
-  <div class="navbar bg-base-100">
-    <div class="dropdown dropdown-hover">
-  {/* <label tabindex="0" class="btn m-1">Hover</label> */}
-  <ul tabindex="0" class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+
+  <div className="navbar bg-base-100">
+    <div className="dropdown dropdown-hover">
+  {/* <label tabindex="0" className="btn m-1">Hover</label> */}
+  <ul tabindex="0" className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
     <li><a>Item 1</a></li>
     <li><a>Item 2</a></li>
   </ul>
-</div>
-  <div class="flex-none">
-    <button class="btn btn-square btn-ghost">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-5 h-5 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+  </div>
+  <div className="flex-none">
+    <button className="btn btn-square btn-ghost">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-9 h-9 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
     </button>
   </div>
-  <div class="flex-1">
-    <h2 class="company-name normal-case text-xl">Company Name</h2>
+  <div className="flex-1">
+    <h2 className="company-name normal-case text-xl">Company Name</h2>
   </div>
-  <div class="flex-none">
-    <button class="btn btn-square btn-ghost">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-5 h-5 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
-    </button>
+  <div className="avatar">
+    <div className="w-24 mask mask-triangle">
+    <img src="https://api.lorem.space/image/face?hash=60857" alt="Company Logo"/>
+    </div>
   </div>
 </div> )
 }
